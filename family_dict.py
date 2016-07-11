ac6466ab0c5850fc7f2cb7965b0930424d0c06fb
Ryan Tanay <ryantanay@newevolution.org>
2016-07-11 14:48:50 -0500

    Switched to using .format for its type conversion

--- family_dict.py
+++ family_dict.py
@@ -1,4 +1,4 @@
 my_family = {'brother': {'name': 'Travis', 'age': 32}, 'sister': {'name': 'Brooke', 'age': 34}}
 
-for k, v in my_family.items():
-    print(' '.join([v['name'], 'is my', k, 'and is', str(v['age']), 'years old']))
+#[print(' '.join([v['name'], 'is my', k, 'and is', str(v['age']), 'years old'])) for k, v in my_family.items()]
+[print('{0} is my {1} and is {2} years old'.format(v['name'], k, v['age'])) for k, v in my_family.items()]
